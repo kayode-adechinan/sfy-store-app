1213f48d396112fca60170eec6ae543ec4fa91c2
Kayode Adechinan <kayode.adechinan@gmail.com>
2022-06-04 14:20:23 +0000

    feat: migrate to mysql

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Input;
+use App\Entity\Output;
 use App\Entity\Product;
+use App\Entity\Role;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -49,5 +54,10 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
         yield MenuItem::linkToCrud('Products', 'fas fa-map-marker-alt', Product::class);
+        yield MenuItem::linkToCrud('Categories', 'fas fa-map-marker-alt', Category::class);
+        yield MenuItem::linkToCrud('Inputs', 'fas fa-map-marker-alt', Input::class);
+        yield MenuItem::linkToCrud('Output', 'fas fa-map-marker-alt', Output::class);
+        yield MenuItem::linkToCrud('Roles', 'fas fa-map-marker-alt', Role::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-map-marker-alt', User::class);
     }
 }
